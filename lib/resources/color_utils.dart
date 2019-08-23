9b001ec435b54da393828e8edd03740b1f1bbb20
Liviu Iuga <liviu.iuga@zipperstudios.co>
2019-08-23 15:03:18 +0300

    Added gray color variations, orange and black

--- lib/resources/color_utils.dart
+++ lib/resources/color_utils.dart
@@ -1,5 +1,18 @@
 import 'dart:ui';
 
 class ColorUtils {
+
+  //Orange
+  static const Color messageOrange = Color(0XFFFB8A40);
+
+  //Gray
   static const Color gray = Color(0xFF801B1C1D);
+  static const Color messageGray = Color(0XFFF5F5F5);
+  static const Color lightGray = Color(0XFF707070);
+  static const Color textGray = Color(0XFFB4B4B4);
+  static const Color darkGray = Color(0XFF404040);
+
+  //Black
+  static const Color almostBlack = Color(0XFF404040);
+  static const Color textBlack = Color(0XFF1B1C1D);
 }
