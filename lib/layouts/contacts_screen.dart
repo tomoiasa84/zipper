4226e92b06a173b70ef4e729298ff8daa02ddb9d
Loredana Petrea <loredana.petrea@zipperstudios.co>
2019-08-26 12:18:10 +0300

    Used the same UI both for contacts items and for users items

--- lib/layouts/contacts_screen.dart
+++ lib/layouts/contacts_screen.dart
@@ -88,70 +88,77 @@ class ContactsScreenState extends State<ContactsScreen> {
                     ],
                   ),
                   Expanded(
-                    child: widget.contacts != null
-                        ? Container(
-                            child: Scrollbar(
-                                child:
-//                        ListView.builder(
-//                            itemCount: widget.contacts?.length ?? 0,
-//                            itemBuilder:
-//                                (BuildContext context, int index) {
-//                              Contact contact =
-//                              widget.contacts.elementAt(index);
-//                              return Container(
-//                                margin: const EdgeInsets.only(
-//                                    left: 12.0, right: 12.0),
-//                                child: Card(
-//                                  child: ListTile(
-//                                    leading: Icon(Icons.person),
-//                                    title:
-//                                    Text(contact.displayName ?? ""),
-//                                  ),
-//                                ),
-//                              );
-//                            }),
-                                    ListView.builder(
-                              itemCount: 2,
-                              itemBuilder: (BuildContext context, int index) {
-                                return FutureBuilder<
-                                    List<Map<String, dynamic>>>(
-                                  future: _contactsBloc.getUsers(),
-                                  builder: (BuildContext context,
-                                      AsyncSnapshot<List<Map<String, dynamic>>>
-                                          snapshot) {
-                                    if (snapshot.connectionState ==
-                                        ConnectionState.waiting) {
-                                      return SizedBox(
-                                        height:
-                                            MediaQuery.of(context).size.height *
-                                                2,
-                                        child: const Align(
-                                            alignment: Alignment.topCenter,
-                                            child: CircularProgressIndicator()),
-                                      );
-                                    }
-                                    if (snapshot.hasError) {
-                                      return Center(
-                                          child: Text(
-                                              'Error : ${snapshot.error}'));
-                                    } else {
-                                      return _buildPage(snapshot.data);
-                                    }
-                                  },
-                                );
-                              },
-                            )),
-                          )
-                        : (_permissionStatus == PermissionStatus.granted
-                            ? Center(
-                                child: CircularProgressIndicator(),
-                              )
-                            : Center(
-                                child: Text(
-                                    "This app requires contacts access to function."))),
+//                    child: widget.contacts != null
+//                        ? Container(
+//                            child: Scrollbar(
+//                              child: _buildContactsListView(),
+//                            ),
+//                          )
+//                        : (_permissionStatus == PermissionStatus.granted
+//                            ? Center(
+//                                child: CircularProgressIndicator(),
+//                              )
+//                            : Center(
+//                                child: Text(
+//                                    "This app requires contacts access to function."))),
+                    child: TabBarView(
+                      children: <Widget>[
+                        _buildContactsListView(),
+                        _buildUsersListView(),
+                        Container()
+                      ],
+                    ),
                   ),
                 ],
               ))),
+    );
+  }
+
+  ListView _buildUsersListView() {
+    return ListView.builder(
+      itemCount: 2,
+      itemBuilder: (BuildContext context, int index) {
+        return FutureBuilder<List<Map<String, dynamic>>>(
+          future: _contactsBloc.getUsers(),
+          builder: (BuildContext context,
+              AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return SizedBox(
+                height: MediaQuery.of(context).size.height * 2,
+                child: const Align(
+                    alignment: Alignment.topCenter,
+                    child: CircularProgressIndicator()),
+              );
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text('Error : ${snapshot.error}'));
+            } else {
+              return _buildPage(snapshot.data);
+            }
+          },
+        );
+      },
+    );
+  }
+
+  ListView _buildContactsListView() {
+    return ListView.builder(
+        itemCount: widget.contacts.length ?? 0,
+        itemBuilder: (BuildContext context, int index) {
+          Contact contact = widget.contacts.elementAt(index);
+          return _buildListItem(contact.displayName);
+        });
+  }
+
+  Container _buildListItem(String name) {
+    return Container(
+      margin: const EdgeInsets.only(left: 12.0, right: 12.0),
+      child: Card(
+        child: ListTile(
+          leading: Icon(Icons.person),
+          title: Text(name ?? ""),
+        ),
+      ),
     );
   }
 
@@ -161,7 +168,7 @@ class ContactsScreenState extends State<ContactsScreen> {
       primary: false,
       children: page.map<Widget>((Map<String, dynamic> item) {
         final User user = User.fromJson(item);
-        return EpisodesCard(episode: user);
+        return _buildListItem(user.name);
       }).toList(),
     );
   }
@@ -181,52 +188,51 @@ class ContactsScreenState extends State<ContactsScreen> {
   }
 }
 
-class EpisodesCard extends StatelessWidget {
-  const EpisodesCard({Key key, this.episode}) : super(key: key);
-
-  final User episode;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.blueAccent,
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.circular(8.0),
-        boxShadow: const <BoxShadow>[
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 8.0,
-            offset: Offset(0.0, 8.0),
-          ),
-        ],
-      ),
-      child: Column(
-        children: <Widget>[
-          Text(
-            episode.name,
-            style: const TextStyle(
-                color: Colors.white70,
-                fontSize: 18,
-                fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            episode.name,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
-                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            episode.name,
-            style: const TextStyle(color: Colors.white70, fontSize: 14),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//class EpisodesCard extends StatelessWidget {
+//  const EpisodesCard({Key key, this.episode}) : super(key: key);
+//
+//  final User episode;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Container(
+//      margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+//      padding: const EdgeInsets.all(8),
+//      decoration: BoxDecoration(
+//        shape: BoxShape.rectangle,
+//        borderRadius: BorderRadius.circular(8.0),
+//        boxShadow: const <BoxShadow>[
+//          BoxShadow(
+//            color: Colors.black12,
+//            blurRadius: 8.0,
+//            offset: Offset(0.0, 8.0),
+//          ),
+//        ],
+//      ),
+//      child: Column(
+//        children: <Widget>[
+//          Text(
+//            episode.name,
+//            style: const TextStyle(
+//                color: Colors.white70,
+//                fontSize: 18,
+//                fontWeight: FontWeight.bold),
+//          ),
+//          const SizedBox(height: 4),
+//          Text(
+//            episode.name,
+//            maxLines: 1,
+//            overflow: TextOverflow.ellipsis,
+//            style: const TextStyle(
+//                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+//          ),
+//          const SizedBox(height: 8),
+//          Text(
+//            episode.name,
+//            style: const TextStyle(color: Colors.white70, fontSize: 14),
+//          ),
+//        ],
+//      ),
+//    );
+//  }
+//}
