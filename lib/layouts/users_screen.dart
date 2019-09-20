05cfbf24c233282b78de21fbcb77475369ae219d
Loredana Petrea <loredana.petrea@zipperstudios.co>
2019-09-20 11:13:26 +0300

    Removed tabs on users screen

--- lib/layouts/users_screen.dart
+++ lib/layouts/users_screen.dart
@@ -52,18 +52,7 @@ class UsersScreenState extends State<UsersScreen> {
     return SafeArea(
       child: ModalProgressHUD(
         inAsyncCall: _saving,
-        child: Scaffold(
-          appBar: _buildAppBar(),
-          body: DefaultTabController(
-            length: 3,
-            child: Column(
-              children: <Widget>[
-                _buildTabBar(),
-                _buildContent(),
-              ],
-            ),
-          ),
-        ),
+        child: Scaffold(appBar: _buildAppBar(), body: _buildUsersListView()),
       ),
     );
   }
@@ -88,116 +77,69 @@ class UsersScreenState extends State<UsersScreen> {
         ]);
   }
 
-  Column _buildTabBar() {
-    return Column(
-      children: <Widget>[
-        new TabBar(
-          labelStyle:
-              TextStyle(fontFamily: "Arial", fontWeight: FontWeight.bold),
-          isScrollable: true,
-          labelColor: ColorUtils.messageOrange,
-          unselectedLabelColor: ColorUtils.darkerGray,
-          indicatorColor: ColorUtils.messageOrange,
-          tabs: _buildTabs(),
-        ),
-        Container(
-          margin: const EdgeInsets.only(left: 21.0, right: 21.0, bottom: 12.0),
-          // Add top border
-          decoration: BoxDecoration(
-            border: Border(
-              top: BorderSide(
-                color: ColorUtils.lightLightGray,
-                width: 0.6,
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Expanded _buildContent() {
-    return Expanded(
-      child: TabBarView(
-        children: <Widget>[_buildUsersListView(), Container(), Container()],
-      ),
-    );
-  }
-
   ListView _buildUsersListView() {
     return ListView.builder(
         itemCount: _usersList?.length ?? 0,
         itemBuilder: (BuildContext context, int index) {
           User user = _usersList.elementAt(index);
-          return _buildListItem(Uint8List(0), user);
+          return Container(
+              margin: EdgeInsets.only(
+                  top: (index == 0) ? 16.0 : 0.0,
+                  bottom: (index == _usersList.length - 1) ? 16.0 : 0.0,
+                  left: 16.0,
+                  right: 16.0),
+              child: _buildListItem(Uint8List(0), user));
         });
   }
 
-  Container _buildListItem(Uint8List image, User user) {
-    return Container(
-      margin: const EdgeInsets.only(left: 16.0, right: 16.0),
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Container(
-          padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
-          child: ListTile(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => UserDetailsScreen(user)));
-            },
-            leading: (image != null && image.length > 0)
-                ? CircleAvatar(backgroundImage: MemoryImage(image))
-                : CircleAvatar(
-                    child: Text(getInitials(user.name),
-                        style: TextStyle(color: ColorUtils.darkerGray)),
-                    backgroundColor: ColorUtils.lightLightGray,
-                  ),
-            title: Row(
-              children: <Widget>[
-                Flexible(
-                  child: Container(
-                      child: Text(
-                    user.name ?? "",
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                        fontFamily: 'Arial', fontWeight: FontWeight.bold),
-                  )),
+  Card _buildListItem(Uint8List image, User user) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Container(
+        padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+        child: ListTile(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => UserDetailsScreen(user)));
+          },
+          leading: (image != null && image.length > 0)
+              ? CircleAvatar(backgroundImage: MemoryImage(image))
+              : CircleAvatar(
+                  child: Text(getInitials(user.name),
+                      style: TextStyle(color: ColorUtils.darkerGray)),
+                  backgroundColor: ColorUtils.lightLightGray,
                 ),
-                Container(
-                  padding: const EdgeInsets.only(left: 4.0),
-                  child: Image.asset(
-                    "assets/images/ic_contacts.png",
-                    height: 16.0,
-                    width: 16.0,
-                  ),
-                )
-              ],
-            ),
-            subtitle: Text(
-              "#installer",
-              style: TextStyle(color: ColorUtils.messageOrange),
-            ),
+          title: Row(
+            children: <Widget>[
+              Flexible(
+                child: Container(
+                    child: Text(
+                  user.name ?? "",
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                      fontFamily: 'Arial', fontWeight: FontWeight.bold),
+                )),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 4.0),
+                child: Image.asset(
+                  "assets/images/ic_contacts.png",
+                  height: 16.0,
+                  width: 16.0,
+                ),
+              )
+            ],
+          ),
+          subtitle: Text(
+            "#installer",
+            style: TextStyle(color: ColorUtils.messageOrange),
           ),
         ),
       ),
     );
-  }
-
-  List<Widget> _buildTabs() {
-    return <Widget>[
-      Tab(
-        text: Localization.of(context).getString('all').toUpperCase(),
-      ),
-      Tab(
-        text: Localization.of(context).getString('lastAccessed').toUpperCase(),
-      ),
-      Tab(
-        text: Localization.of(context).getString('favorites').toUpperCase(),
-      ),
-    ];
   }
 }
