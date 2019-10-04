4cfa7cb8b547c78d47b54da22aeb8b5fbc47c2ee
Loredana Petrea <loredana.petrea@zipperstudios.co>
2019-10-04 13:59:56 +0300

    Fixed issue where cards are not sent in chat.

--- lib/model/recommand.dart
+++ lib/model/recommand.dart
@@ -3,12 +3,25 @@ import 'package:contractor_search/model/user.dart';
 import 'card.dart';
 
 class Recommend {
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'card': card,
+        'userAsk': userAsk,
+        'userSend': userSend,
+        'userRecommand': userRecommend,
+        'acceptedFlag': acceptedFlag,
+      };
+
   Recommend.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         card = json['card'] != null ? CardModel.fromJson(json['card']) : null,
-        userAsk = json['userAsk'] != null ? User.fromJson(json['userAsk']) : null,
-        userSend = json['userSend'] != null ? User.fromJson(json['userSend']) : null,
-        userRecommend = json['userRecommand'] != null ? User.fromJson(json['userRecommand']) : null,
+        userAsk =
+            json['userAsk'] != null ? User.fromJson(json['userAsk']) : null,
+        userSend =
+            json['userSend'] != null ? User.fromJson(json['userSend']) : null,
+        userRecommend = json['userRecommand'] != null
+            ? User.fromJson(json['userRecommand'])
+            : null,
         acceptedFlag = json['acceptedFlag'];
 
   final int id;
