c0d94d9842ef0671b84b941d224e6bd4079cae0c
Liviu Iuga <liviu.iuga@zipperstudios.co>
2019-10-07 16:39:13 +0300

    Removed loadmore library, implemented custom pagination. Library was not working as expected, introducing a bug, loadmore progress bar always present even if chat did not finish loading all the messages

--- lib/layouts/chat_screen.dart
+++ lib/layouts/chat_screen.dart
@@ -15,12 +15,10 @@ import 'package:contractor_search/models/WrappedMessage.dart';
 import 'package:contractor_search/resources/color_utils.dart';
 import 'package:contractor_search/resources/localization_class.dart';
 import 'package:contractor_search/utils/custom_dialog.dart';
-import 'package:contractor_search/utils/custom_load_more_delegate.dart';
 import 'package:contractor_search/utils/general_methods.dart';
 import 'package:contractor_search/utils/general_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:loadmore/loadmore.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -35,14 +33,14 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
-  final ScrollController _listScrollController = new ScrollController();
+  final ScrollController _controller = new ScrollController();
   final List<Object> _listOfMessages = new List();
   final ChatBloc _chatBloc = ChatBloc();
   PubNubConversation _pubNubConversation;
   StreamSubscription _subscription;
   User _interlocutorUser;
   User _currentUser;
-  bool _loading = false;
+  bool _loading = true;
 
   final TextEditingController _textEditingController =
       new TextEditingController();
@@ -137,6 +135,21 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
     WidgetsBinding.instance.addObserver(this);
     _pubNubConversation = widget.pubNubConversation;
     _initScreen();
+    _controller.addListener(_scrollListener);
+    _loadMore().then((onValue) {
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
+
+  _scrollListener() {
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      setState(() {
+        _loadMore();
+      });
+    }
   }
 
   @override
@@ -194,7 +207,7 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
 
   Future<bool> _loadMore() async {
     if (_pubNubConversation == null) {
-      return true;
+      return false;
     }
     if (_chatBloc.historyStart != 0) {
       await _chatBloc
@@ -351,25 +364,19 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
   }
 
   Widget getListView(List<Object> listOfMessages) {
-    var listView = LoadMore(
-      delegate: CustomLoadMoreDelegate(context),
-      isFinish: _chatBloc.historyStart == 0 || _chatBloc.stopFetchingMessages(),
-      onLoadMore: _loadMore,
-      child: ListView.builder(
-        reverse: true,
-        padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-        itemBuilder: (context, position) {
-          var item = listOfMessages[position];
-          return GestureDetector(
-            child: _selectMessageLayout(item, position),
-            onTap: () => _goToImagePreview(item),
-          );
-        },
-        itemCount: listOfMessages.length,
-        controller: _listScrollController,
-      ),
+    return ListView.builder(
+      reverse: true,
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+      itemBuilder: (context, position) {
+        var item = listOfMessages[position];
+        return GestureDetector(
+          child: _selectMessageLayout(item, position),
+          onTap: () => _goToImagePreview(item),
+        );
+      },
+      itemCount: listOfMessages.length,
+      controller: _controller,
     );
-    return listView;
   }
 
   void _goToImagePreview(Object item) {
