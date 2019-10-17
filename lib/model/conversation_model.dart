89ee36a40fe1958908a3c54925eb7d536116050e
Liviu Iuga <liviu.iuga@zipperstudios.co>
2019-10-17 14:54:13 +0300

    Check for user null in conversations

--- lib/model/conversation_model.dart
+++ lib/model/conversation_model.dart
@@ -1,10 +1,10 @@
 import 'package:contractor_search/model/user.dart';
 
-class ConversationModel{
+class ConversationModel {
   ConversationModel.fromJson(Map<String, dynamic> json)
       : id = json['id'],
-        user1 = User.fromJson(json['user1']),
-        user2 = User.fromJson(json['user2']);
+        user1 = json['user1'] != null ? User.fromJson(json['user1']) : null,
+        user2 = json['user2'] != null ? User.fromJson(json['user2']) : null;
 
   final String id;
   final User user1;
