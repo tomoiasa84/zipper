02013fc90d4993068f6b8104278efe100d2c1382
Tudor Adrian <itudor.adrian@gmail.com>
2019-11-21 16:24:52 +0200

    search by tag

--- lib/utils/search_users_util.dart
+++ lib/utils/search_users_util.dart
@@ -1,3 +1,4 @@
+import 'package:contractor_search/model/tag.dart';
 import 'package:contractor_search/model/user.dart';
 import 'package:contractor_search/resources/color_utils.dart';
 import 'package:flutter/material.dart';
@@ -41,14 +42,27 @@ class UserSearch extends SearchDelegate<String> {
   Widget buildResults(BuildContext context) {
     return Container();
   }
-
+//  Iterable<User> getQueryResult(String queryString){
+//    List<User> returnList;
+//    _users.forEach((user)=>{
+//
+//      //user.tags.where((tag)=>tag.name.toLowerCase().startsWith(queryString.toLowerCase()))
+//      if(user.name.toLowerCase().startsWith(queryString.toLowerCase())){
+//        returnList.add(user);
+//      }
+//
+//    });
+//    return returnList;
+//  }
   @override
   Widget buildSuggestions(BuildContext context) {
     final suggestionList = query.isEmpty
         ? _users
         : _users.where(
-            (user) => user.name.toLowerCase().startsWith(query.toLowerCase()));
-
+            (user) => user.name.toLowerCase().startsWith(query.toLowerCase())||(
+                user.tags.where((usertag)=>usertag.tag.name.toLowerCase().startsWith(query.toLowerCase())).toList().length != 0
+            ));
+    //user.tags.where( (tag) => tag.name.startsWith(query.toLowerCase()))
     return ListView.builder(
       itemBuilder: (context, index) => ListTile(
         onTap: () {
@@ -87,7 +101,11 @@ class UserSearch extends SearchDelegate<String> {
                   text: suggestionList
                       .elementAt(index)
                       .name
-                      .substring(0, query.length),
+                      .substring(0, query.length <= suggestionList
+                      .elementAt(index)
+                      .name.length ? query.length : suggestionList
+                      .elementAt(index)
+                      .name.length),
                   style: TextStyle(
                       color: Colors.black, fontWeight: FontWeight.bold),
                   children: [
@@ -95,7 +113,11 @@ class UserSearch extends SearchDelegate<String> {
                         text: suggestionList
                             .elementAt(index)
                             .name
-                            .substring(query.length),
+                            .substring(query.length <= suggestionList
+                            .elementAt(index)
+                            .name.length ? query.length : suggestionList
+                            .elementAt(index)
+                            .name.length),
                         style: TextStyle(color: Colors.grey))
                   ]),
             ),
